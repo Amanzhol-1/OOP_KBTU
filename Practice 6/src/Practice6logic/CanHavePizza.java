ac6902341d909165116c26404254ca320cc2fc2b
1ncrease <69354535+1ncrease@users.noreply.github.com>
2023-11-19 13:23:01 +0600

    6
    6

--- Practice 6/src/Practice6logic/CanHavePizza.java
+++
@@ -1,8 +0,0 @@
-package Practice6logic;
-
-public interface CanHavePizza {
-    void eatPizza();
-    default void pizzaWeight() {
-        System.out.println("Pizza weight left: " + Pizza.getWeight());
-    }
-}
--- Practice 6/src/Practice6logic/Speak.java
+++
@@ -1,9 +0,0 @@
-package Practice6logic;
-
-public interface Speak {
-    void speak();
-
-    default void isSpeak() {
-        System.out.println("I can speak");
-    }
-}
